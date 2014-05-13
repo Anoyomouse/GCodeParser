0d29ba8e438d958f03ea49dd3ccba94b44bbf9ff
David-John Miller <anoyomouse@petermiller.co.za>
2014-05-13 21:27:57 +0200

    Added color control, Various fixes

---
+++ ControlSuite/ColorPickerBox.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ControlSuite
+{
+	partial class ColorPickerBox
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.txtHex = new System.Windows.Forms.TextBox();
+            this.cmdSelect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtHex
+            // 
+            this.txtHex.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtHex.Location = new System.Drawing.Point(0, 0);
+            this.txtHex.Name = "txtHex";
+            this.txtHex.Size = new System.Drawing.Size(100, 20);
+            this.txtHex.TabIndex = 0;
+            this.txtHex.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // cmdSelect
+            // 
+            this.cmdSelect.Dock = System.Windows.Forms.DockStyle.Right;
+            this.cmdSelect.Location = new System.Drawing.Point(100, 0);
+            this.cmdSelect.Name = "cmdSelect";
+            this.cmdSelect.Size = new System.Drawing.Size(20, 23);
+            this.cmdSelect.TabIndex = 1;
+            this.cmdSelect.UseVisualStyleBackColor = true;
+            this.cmdSelect.Click += new System.EventHandler(this.cmdSelect_Click);
+            // 
+            // ColorPickerBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.txtHex);
+            this.Controls.Add(this.cmdSelect);
+            this.Name = "ColorPickerBox";
+            this.Size = new System.Drawing.Size(120, 23);
+            this.Load += new System.EventHandler(this.ColorPickerBox_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox txtHex;
+		private System.Windows.Forms.Button cmdSelect;
+	}
+}
---
+++ ControlSuite/ColorPickerBox.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ControlSuite
+{
+	public partial class ColorPickerBox : UserControl
+	{
+		public ColorPickerBox()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnSizeChanged(EventArgs e)
+		{
+			#region Code
+			if (this.Height != txtHex.Height)
+			{
+				this.Height = txtHex.Height;
+				cmdSelect.Height = txtHex.Height;
+			}
+			base.OnSizeChanged(e);
+			#endregion
+		}
+
+		Color _my_color = Color.White;
+
+		private void cmdSelect_Click(object sender, EventArgs e)
+		{
+			#region Code
+			ColorDialog cd = new ColorDialog();
+			cd.AnyColor = true;
+			cd.AllowFullOpen = true;
+			cd.FullOpen = true;
+			cd.SolidColorOnly = false;
+			cd.Color = _my_color;
+			var res = cd.ShowDialog(this);
+			if (res != DialogResult.OK)
+				return;
+
+			_my_color = cd.Color;
+			txtHex.BackColor = _my_color;
+			#endregion
+		}
+
+		public Color SelectedColor
+		{
+			get
+			{
+				return _my_color;
+			}
+			set
+			{
+				_my_color = value;
+				txtHex.BackColor = value;
+			}
+		}
+
+		private void ColorPickerBox_Load(object sender, EventArgs e)
+		{
+			#region Code
+			if (_my_color == System.Drawing.Color.Empty)
+				_my_color = Color.White;
+
+			txtHex.BackColor = _my_color;
+			#endregion
+		}
+	}
+}
